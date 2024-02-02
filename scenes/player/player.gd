5af1828829cd5b7c3fee5f96c12278edd99552a3
Gabriel Cardoso <gabrielplinboy@gmail.com>
2024-02-02 17:07:54 -0300

    Fixed shooting laser and now shoot grenade

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal laser(pos)
-signal grenade(pos)
+signal laser(pos, direction)
+signal grenade(pos, direction)
 
 var speed = 500;
 
@@ -13,23 +13,29 @@ func _process(_delta):
 	
 	#Input
 	var direction = Input.get_vector("left", "right", "up", "down")
+	var player_direction = (get_global_mouse_position() - position).normalized()
 	velocity = direction * speed
 	move_and_slide()
+
+	#Rotate
+	look_at(get_global_mouse_position())
 
 	#Shoot
 	if Input.is_action_pressed("primary action") and can_laser:
 		var laser_markers = $ShootStartPositions.get_children()
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
+		
 		$ShootLReloadTimer.start()
-		laser.emit(selected_laser.global_position)
+		laser.emit(selected_laser.global_position, player_direction)
 		
 	#Grenade
 	if Input.is_action_pressed("secondary action") and can_grenade:
 		var pos = $ShootStartPositions.get_children()[0].global_position
 		can_grenade = false
+		
 		$GrenadeReloadTimer.start()
-		grenade.emit(pos)
+		grenade.emit(pos, player_direction)
 
 func _on_timer_timeout():
 	can_laser = true	
