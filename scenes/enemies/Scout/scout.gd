986f6ce68459a62cf2609597bfb948667f3b37d6
Gabriel Cardoso <gabrielplinboy@gmail.com>
2024-02-12 22:35:06 -0300

    Scout health

--- scenes/enemies/Scout/scout.gd
+++ scenes/enemies/Scout/scout.gd
@@ -3,10 +3,17 @@ extends CharacterBody2D
 signal laser(pos, direction)
 var player_nearby: bool = false
 var can_laser: bool = true
+var vulnerable: bool = true
 var right_gun_use: bool = true
+var health: int = 30
 
 func hit():
-	print("scout has hit")
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		$Timers/HitTimer.start()
+	if health <= 0:
+		queue_free()
 
 func _process(_delta):
 	if player_nearby:
@@ -18,7 +25,7 @@ func _process(_delta):
 			var direction: Vector2 = (Globals.player_pos - position).normalized()
 			laser.emit(pos, direction)
 			can_laser = false
-			$LaserCooldown.start()
+			$Timers/LaserTimer.start()
 
 func _on_attack_area_body_entered(_body):
 	player_nearby = true
@@ -26,6 +33,8 @@ func _on_attack_area_body_entered(_body):
 func _on_attack_area_body_exited(_body):
 	player_nearby = false
 
-
-func _on_laser_cooldown_timeout():
+func _on_laser_timer_timeout():
 	can_laser = true
+
+func _on_hit_timer_timeout():
+	vulnerable = true
