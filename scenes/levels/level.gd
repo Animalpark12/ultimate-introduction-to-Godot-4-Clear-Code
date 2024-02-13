2e4829145fcc9531866feedb7f313b44f6ec489e
Gabriel Cardoso <gabrielplinboy@gmail.com>
2024-02-12 21:30:34 -0300

    More of inside level

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -21,21 +21,9 @@ func _on_player_laser(pos, direction):
 	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
 	laser.direction = direction
 	$Projectiles.add_child(laser)
-	$UI.updade_laser_text()
 
 func _on_player_grenade(pos, direction):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
-	$UI.update_grenade_text()
-
-
-func _on_house_player_entered():
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1).set_trans(Tween.TRANS_CUBIC)
-
-
-func _on_house_player_exited():
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.5,0.5), 1).set_trans(Tween.TRANS_CUBIC)
--- scenes/levels/outside/outside.gd
+++ scenes/levels/outside/outside.gd
@@ -5,3 +5,12 @@ func _on_gate_player_entered_gate(_body):
 	var tween = create_tween()	
 	tween.tween_property($Player, "speed", 0, 0.5)
 	TransitionLayer.change_scene("res://scenes/levels/inside/inside.tscn")
+
+func _on_house_player_entered():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1).set_trans(Tween.TRANS_CUBIC)
+
+
+func _on_house_player_exited():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.5,0.5), 1).set_trans(Tween.TRANS_CUBIC)
